a23bea69ecbce72fc5e0578475b537f730e2fbb9
capstonefinal <capstonefinal1234@gmail.com>
2022-09-27 17:31:13 +0800

    updated axiso base url

--- axiosConfig.js
+++ axiosConfig.js
@@ -1,4 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'http://localhost:5000/api/';
+axios.defaults.baseURL = 'https://capstone-attendance-backend.onrender.com/api/';
+// axios.defaults.baseURL = 'http://localhost:5000/api/';
 export default axios.create()
