5a8af172c6ca8d8e1a0d3fd876ceec8f02e4534c
capstonefinal <113873411+capstonefinal@users.noreply.github.com>
2022-10-25 15:21:30 +0800

    axios config: fix if no localstorage user

--- axiosConfig.js
+++ axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 axios.defaults.baseURL = 'https://capstone-attendance-backend.onrender.com/api/';
-axios.defaults.headers.common['Authorization'] = 'Bearer '+JSON.parse(localStorage.getItem('user')).token;
+axios.defaults.headers.common['Authorization'] = 'Bearer '+JSON.parse(localStorage.getItem('user'))?.token;
 // axios.defaults.baseURL = 'http://localhost:5000/api/';
 export default axios.create()
